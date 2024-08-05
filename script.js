f205fd022c170cb74b97091349d9ba0a16580237
ZazaIphone <zazakhazhalia@yahoo.com>
2024-08-06 02:05:30 +0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+document.getElementById('contactForm').addEventListener('submit', function(event) {
+                        event.preventDefault();
+                        alert('Ваше сообщение было отправлено!');
+                    });
+                    
